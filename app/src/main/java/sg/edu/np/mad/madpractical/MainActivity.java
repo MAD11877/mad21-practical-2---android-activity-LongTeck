8c5628e949ded5c895faea88d38fd12b16c6852a
Long_Teck <longteck@gmail.com>
2022-05-24 10:55:52 +0800

    Edited textview changes

--- app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView name = findViewById(R.id.Name);
+        name.setText(user.Name);
+
+        TextView desc = findViewById(R.id.Decription);
+        desc.setText(user.Description);
+
     }
 
     public void OnClick(View v){
